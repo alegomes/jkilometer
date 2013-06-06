02cf19f1d968111d29600eaf4f47b4e7f857f812
Alexandre Gomes <alexandre.gomes@seatecnologia.com.br>
2013-06-06 17:47:34 -0300

    file included

---
+++ analyse_by_column.rb
@@ -0,0 +1,59 @@
+input=ARGV[0]
+field=ARGV[1].to_i
+
+if ARGV.empty?
+  puts 
+  puts "Usage: analyse_by_column <summary_results.csv> <column_number>"
+  puts
+  exit
+end
+
+last_line_was_comment=false
+last_line_was_data=false
+
+input_file = File.open(input, "r")
+
+description=""
+timestamp=""
+data=""
+
+puts "Timestamp;Description;Column #{field} data serie"
+input_file.each do |line| 
+
+  line.chomp!
+
+  # Comment
+  if /#.*$/ =~ line
+    if last_line_was_data
+      puts "#{timestamp};\"#{description}\";#{data}" 
+      description=""
+      timestamp=""
+      data=""
+    end
+
+    if /#[\s]+(.+)$/ =~ line
+      description=$1
+    end
+
+    last_line_was_comment=true
+    last_line_was_data=false
+  end
+
+  # Data
+  if /[\d]{2}\/[\d]{2}\/[\d]{4}.*$/ =~ line
+
+    line = line.split(";")
+
+    if last_line_was_comment
+      timestamp=line[0]
+      data=line[field]
+    else
+      data=data+";"+line[field]
+    end
+
+    last_line_was_comment=false
+    last_line_was_data=true
+
+  end
+
+end
